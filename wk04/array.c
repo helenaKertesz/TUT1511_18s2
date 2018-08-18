fa61dbfba36b5449def8ed3c27c9321acc4e61cc
helenaKertesz <helena.kertesz1@gmail.com>
2018-08-19 09:47:46 +1000

    wk04 more detail on arrays

--- wk04/array.c
+++ wk04/array.c
@@ -6,18 +6,27 @@
 
 int main(void){
 
-   // a few ways of making arrays
-   int array1[SIZE] = {1, 2, 3, 4, 5} // I can initialise each element in an array using a list in curly braces
-                                      // note the use of the #define for the size of the array
+    // a few ways of making arrays
+    int array1[SIZE] = {1, 2, 3, 4, 5} // I can initialise each element in an array using a list in curly braces
+    // note the use of the #define for the size of the array
 
-   int array2[SIZE] = {0};            // if I just have a zero in the curly braces it will set all the values to zero
+    int array2[SIZE] = {0};            // if I just have a zero in the curly braces it will set all the values to zero
 
-   // what's wrong here
-   int numbers[SIZE] = {0};
-   int x = numbers[20];     // since indexing starts from 0, this is actually one past the end of the array
+    // we can access elements in arrays by index
+    array1[0] = 1;     // this sets the first element in this array to 1
+    int i = array2[3]; // this stores the 4th item in array "array2" in the variable i
+                       // it's the 4th element not the 3rd because indexes start at 0
 
-   // let's take it to another dimension!!
-   int more_numbers[SIZE][SIZE] // this is a 2D array, which exists as an array of arrays.
-   
-   return 0;
+    // what's wrong here
+    int numbers[SIZE] = {0};
+    int x = numbers[SIZE];     // since indexing starts from 0, this is actually one past the end of the array (ie the 21st element)
+
+    // let's take it to another dimension!!
+    int more_numbers[SIZE][SIZE]; // this is a 2D array, which exists as an array of arrays.
+                                  // in this case it's an array of 20 elements, where each element is annother array of 20 elements
+
+    more_numbers[0][0] // index access is the same as with 1D arrays 
+                       // this gets us the first element in the first row of this array
+
+    return 0;
 }
